26e0afdc131c1c68410ddb02d4f5d4a454781886
tiago-fullstack2025 <tiago.fullstack2025@gmail.com>
2025-09-30 14:20:34 -0300

    Cria e estiliza index básica

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fly By Night - Gerenciamento</title>
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+    <h1>Fly By Night - CRUD com PHP e SQL</h1>
+    <h2>Gerenciar</h2>
+    <ul>
+        <li><a href="">Fornecedores</a></li>
+        <li><a href="">Produtos</a></li>
+    </ul>
+</body>
+</html>
