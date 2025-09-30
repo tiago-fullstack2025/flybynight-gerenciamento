949d2746d2e320df81a627014d70ec1d9229b035
tiago-fullstack2025 <tiago.fullstack2025@gmail.com>
2025-09-30 14:31:09 -0300

    Adiciona página fornecedores/listar

---
+++ fornecedores/listar.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listar Fornecedores</title>
+    <link rel="stylesheet" href="../css/estilos.css">
+</head>
+<body>
+    <h1>Fornecedores</h1>
+    <a href="">+ Novo fornecedor</a>
+    <a href="../index.php">← Voltar</a>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <h1>Fly By Night - CRUD com PHP e SQL</h1>
     <h2>Gerenciar</h2>
     <ul>
-        <li><a href="">Fornecedores</a></li>
+        <li><a href="fornecedores/listar.php">Fornecedores</a></li>
         <li><a href="">Produtos</a></li>
     </ul>
 </body>
