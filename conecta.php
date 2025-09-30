51d77d297a1a9444cbb5fce77bb3e4a6d2ebf30c
tiago-fullstack2025 <tiago.fullstack2025@gmail.com>
2025-09-30 15:51:31 -0300

    Comenta conecta

--- conecta.php
+++ conecta.php
@@ -9,14 +9,19 @@ $banco = "flybynight_tiago";
 $usuario = "root"; // padrão no XAMPP
 $senha = ""; // padrão no XAMPP
 
+/* try/catch
+Bloco condicional relacionado a verificações de erro */
 try {
+    // Criando um objeto de conexão usando a classe PDO (driver de acesso a BD)
     $conexao = new PDO(
         "mysql:host=$servidor;dbname=$banco;charset=utf8", 
         $usuario, $senha
     );
 
+    // Habilitando a exibição de erros
     $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+    // Configurando o modo de busca de dados para o formato ARRAY ASSOCIATIVO
     $conexao->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch(PDOException $erro){
     die("Erro ao conectar: ".$erro->getMessage());
