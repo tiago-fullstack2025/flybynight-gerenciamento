3a7e918384e0eb3fad87ebf339ca90a0d261ff7a
tiago-fullstack2025 <tiago.fullstack2025@gmail.com>
2025-09-30 15:52:06 -0300

    Programa buscarFornecedores

---
+++ src/fornecedor_crud.php
@@ -0,0 +1,19 @@
+<?php
+/* Neste arquivo teremos todas as funções que serão usadas
+para manipulação (CRUD) de Fornecedores em nosso sistema
+e banco de dados. */
+
+// Acessando o script de conexão ao BD
+require_once "../conecta.php";
+
+/* Retornará um array associativo com os fornecedores  */
+function buscarFornecedores($conexao){
+    // Montamos o comando SQL para a consulta
+    $sql = "SELECT * FROM fornecedores";
+
+    // Executamos o comando e guardamos o resultado da consulta
+    $consulta = $conexao->query($sql);
+
+    // Retornamos o resultado em formato de Array Associativo
+    return $consulta->fetchAll();
+}
