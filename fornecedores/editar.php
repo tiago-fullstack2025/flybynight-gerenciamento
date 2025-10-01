a059a4703ee8c271432fbbb9d3f21eded5c233af
tiago-fullstack2025 <tiago.fullstack2025@gmail.com>
2025-10-01 14:28:34 -0300

    Create editar.php

---
+++ fornecedores/editar.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Editar fornecedor</title>
+    <link rel="stylesheet" href="../css/estilos.css">
+</head>
+<body>
+    <h1>Editar fornecedor</h1>
+
+    <form action="" method="post">
+        <div>
+            <label for="nome">Nome:</label>
+            <input type="text" name="nome" id="nome" required>
+        </div>
+        <button type="submit">Atualizar</button>
+    </form>
+
+    <a href="listar.php">← Voltar</a>
+
+</body>
+</html>
