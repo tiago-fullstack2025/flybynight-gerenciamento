bfc6f1114a2c2313452ada267879366ef5532767
tiago-fullstack2025 <tiago.fullstack2025@gmail.com>
2025-10-09 14:31:30 -0300

    Adiciona função buscarProdutos

--- src/produto_crud.php
+++ src/produto_crud.php
@@ -4,5 +4,14 @@
 require_once "../conecta.php";
 
 function buscarProdutos($conexao){
+    $sql = "SELECT
+                produtos.id, produtos.nome,
+                produtos.preco, produtos.quantidade,
+                fornecedores.nome
+            FROM produtos JOIN fornecedores
+            ON fornecedores.id = produtos.fornecedor_id
+            ORDER BY produtos.nome";
 
+    $consulta = $conexao->query($sql);
+    return $consulta->fetchAll();
 }
