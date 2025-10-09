590545027c591ad175f1d639e0f656e580496fee
tiago-fullstack2025 <tiago.fullstack2025@gmail.com>
2025-10-09 16:00:21 -0300

    Adiciona campos ao form e buscaFornecedores

--- produtos/inserir.php
+++ produtos/inserir.php
@@ -1,36 +1,56 @@
 <?php
 require_once "../src/fornecedor_crud.php";
-
-/* Se o formulário com método post for acionado */
-if( $_SERVER['REQUEST_METHOD'] === 'POST' ){
-    // Então vamos pegar o valor do campo chamado nome (via atributo NAME)
-    $nome = $_POST['nome'];
-    
-    // Chamamos a função, passamos os dados de conexão e o valor do nome digitado
-    inserirFornecedor($conexao, $nome);
-
-    // Redirecionamos para a página listar.php
-    header("location:listar.php");
-    exit;
-}
+$fornecedores = buscarFornecedores($conexao);
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inserir fornecedor</title>
+    <title>Inserir produto</title>
     <link rel="stylesheet" href="../css/estilos.css">
 </head>
 <body>
-    <h1>Adicionando um novo fornecedor</h1>
+    <h1>Adicionando um novo produto</h1>
 
     <form action="" method="post">
         <div>
             <label for="nome">Nome:</label>
-            <!-- atributo required indica um campo obrigatório -->
             <input type="text" name="nome" id="nome" required>
         </div>
+        <div>
+            <label for="descricao">Descrição:</label>
+            <textarea name="descricao" id="descricao" rows="5"></textarea>
+        </div>
+
+        <div>
+            <label for="preco">Preço:</label>
+            <input type="number" name="preco" id="preco" required min="0" step="0.01">
+        </div>
+
+        <div>
+            <label for="quantidade">Quantidade:</label>
+            <input type="number" name="quantidade" id="quantidade" required min="0">
+        </div>
+
+        <div>
+            <label for="fornecedor">Fornecedor:</label>
+            
+            <select name="fornecedor" id="fornecedor">
+                <!-- Sempre mantenha um option vazio.
+                É o usuário que deve vir aqui escolher. -->
+                <option value=""></option>
+
+                <?php foreach($fornecedores as $fornecedor): ?>
+                <option value="<?=$fornecedor['id']?>">
+                    <?=$fornecedor['nome']?>
+                </option>
+                <?php endforeach; ?>
+
+            </select>
+
+        </div>
+
         <button type="submit">Salvar</button>
     </form>
 
